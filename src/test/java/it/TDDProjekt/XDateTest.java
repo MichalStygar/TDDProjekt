144060aab685da69ceeeea6d597ba1079692cc3e
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 18:27:24 +0100

    Add new test for get value month

--- src/test/java/it/TDDProjekt/XDateTest.java
+++ src/test/java/it/TDDProjekt/XDateTest.java
@@ -9,4 +9,10 @@ public class XDateTest {
 		XDate date = new XDate("2022-01-14");
 		assertEquals(14, date.getDay());
 	}
+
+    @Test
+	public void getMonthTest() throws Exception {
+		XDate date = new XDate("2022-01-14");
+		assertEquals(1, date.getMonth());
+	}
 }
