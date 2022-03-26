6f780e2991346fb7cbf71a123069aad09e3cdc31
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 18:53:32 +0100

    Create getName in Employee class

--- src/main/java/it/TDDProjekt/Employee.java
+++ src/main/java/it/TDDProjekt/Employee.java
@@ -1,5 +1,19 @@
 package it.TDDProjekt;
-
+import java.text.ParseException;
 public class Employee {
-    
+    private String name;
+	private String surName;
+	private String date;
+	private String email;
+	
+	public Employee(String name, String surName, String date, String email) throws ParseException {
+		this.name = name;
+		this.surName = surName;
+		this.date = date;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
--- src/test/java/it/TDDProjekt/EmployeeTest.java
+++ src/test/java/it/TDDProjekt/EmployeeTest.java
@@ -1,8 +1,10 @@
 package it.TDDProjekt;
-
+import static org.junit.Assert.*;
+import org.junit.*;
 public class EmployeeTest {
+    
     @Test
-	public void getMonthTest() throws Exception {
+	public void getNameTest() throws Exception {
 		Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
 		assertEquals("Jan", employee.getName());
 	}
