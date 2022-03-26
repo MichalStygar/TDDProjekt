8b4af3d37208e048eace0826c588ad7da20a6b55
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 19:47:10 +0100

    Create function sendEmail

--- src/main/java/it/TDDProjekt/EmailService.java
+++ src/main/java/it/TDDProjekt/EmailService.java
@@ -1,5 +1,43 @@
 package it.TDDProjekt;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 public class EmailService {
-    
+
+    public void sendEmail(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
+		
+        Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
+        System.out.println(xDate.getDate());
+        String recipient = employee.getEmail();
+        String body = "Mr. "+employee.getSurName()+" "+employee.getName()  +", the subscription to movies ends tomorrow."
+                + "Your account is: "+employee.getEmail()+"."+" Expiration date: "+ (xDate.getDay()+1)+"-0"+xDate.getMonth()+"-"+xDate.getYear();
+        String subject = "End of subscription";
+        
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getInstance(props, null);
+
+        // Construct the message
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("sender@here.com"));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        // Send the message
+        Transport.send(msg);
+        System.out.println("Message sent!!!");
+
+        
+		
+	}
 }
