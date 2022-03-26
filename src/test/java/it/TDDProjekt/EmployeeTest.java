f09a01693b024fe509a8d91f790f79b3711a505d
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 18:54:07 +0100

    Create getSurNameTest

--- src/test/java/it/TDDProjekt/EmployeeTest.java
+++ src/test/java/it/TDDProjekt/EmployeeTest.java
@@ -8,4 +8,10 @@ public class EmployeeTest {
 		Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
 		assertEquals("Jan", employee.getName());
 	}
+
+    @Test
+	public void getSurNameTest() throws Exception {
+		Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
+		assertEquals("Nowak", employee.getSurName());
+	}
 }
