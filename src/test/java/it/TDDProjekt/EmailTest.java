5a0a7263e5f73e00378f24929b53781c68fd060d
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 19:34:54 +0100

    Create willSendEmailTest

--- src/test/java/it/TDDProjekt/EmailTest.java
+++ src/test/java/it/TDDProjekt/EmailTest.java
@@ -1,5 +1,37 @@
 package it.TDDProjekt;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import com.dumbster.smtp.*;
 
 public class EmailTest {
-    
+    private static final int PORT = 9999;
+	private EmailService emailService;
+	private SimpleSmtpServer mailServer;
+
+	@Before
+	public void setUp() throws Exception {
+		mailServer = SimpleSmtpServer.start(PORT);
+		emailService = new EmailService();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mailServer.stop();
+		Thread.sleep(200);
+	}
+
+	@Test
+	public void willSendEmailTest() throws Exception {
+
+		emailService.sendEmail("employee_data.txt", new XDate("2022-03-24"), "localhost", PORT);
+		
+		assertEquals("message not sent?", 1, mailServer.getReceivedEmailSize());
+		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmail().next();
+		assertEquals("Mr. Nowak Jan, the subscription to movies ends tomorrow.Your account is: jan@nowak.com. Expiration date: 25-03-2022", message.getBody());
+		assertEquals("End of subscription", message.getHeaderValue("Subject"));
+		String[] recipients = message.getHeaderValues("To");
+		assertEquals(1, recipients.length);
+		assertEquals("jan@nowak.com", recipients[0].toString());
+	}
 }
