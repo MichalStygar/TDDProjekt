d219c4e4b76c8af86a368651d40e106185a473b6
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-03-26 18:55:57 +0100

    Create getDateTest

--- src/test/java/it/TDDProjekt/EmployeeTest.java
+++ src/test/java/it/TDDProjekt/EmployeeTest.java
@@ -14,4 +14,10 @@ public class EmployeeTest {
 		Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
 		assertEquals("Nowak", employee.getSurName());
 	}
+
+    @Test
+	public void getDateTest() throws Exception {
+		Employee employee = new Employee("Jan", "Nowak", "2022-01-14", "jan@nowak.com");
+		assertEquals("2022-01-14", employee.getDate());
+	}
 }
