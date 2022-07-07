9c102853b00f4fdfaf543a215bca152dd61f2c70
Michał Stygar <w65308@student.wsiz.edu.pl>
2022-07-07 20:02:46 +0200

    SendEmail functions were called in main

--- src/main/java/it/TDDProjekt/Main.java
+++ src/main/java/it/TDDProjekt/Main.java
@@ -1,10 +1,16 @@
 package it.TDDProjekt;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
+		EmailService service = new EmailService();
+		service.sendEmail("C:\\Users\\micha\\Documents\\TDD-Projekt\\TDDProjekt\\employee_data.txt", new XDate(), "localhost", 25);
 	}
 
 }
